d1cfae98f475f6bf160f8ce73241d806f18af699
Xin Du <duxins@gmail.com>
2016-05-25 22:13:40 +0800

    [100-same-tree] Add iterative solution

--- 100-same-tree.cpp
+++ 100-same-tree.cpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <gtest/gtest.h>
 #include <tree/tree.h>
+#include <stack>
 
 using namespace std;
 
@@ -20,6 +21,41 @@ public:
         if(p == NULL || q == NULL) return false;
         if(p->val != q->val) return false;
         return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+
+    //Iterative solution
+    bool isSameTree2(TreeNode* p, TreeNode* q) {
+        stack<TreeNode *> sp;
+        stack<TreeNode *> sq;
+
+        if(p) sp.push(p);
+        if(q) sq.push(q);
+
+        while(!sp.empty() && !sq.empty()){
+            TreeNode *m = sp.top();
+            TreeNode *n = sq.top();
+            sp.pop();
+            sq.pop();
+
+            if(m->val != n->val) return false;
+
+            if(m->left && n->left) {
+                sp.push(m->left);
+                sq.push(n->left);
+            }else if(m->left || n->left){
+                return false;
+            }
+
+            if(m->right && n->right) {
+                sp.push(m->right);
+                sq.push(n->right);
+            }else if(m->right || n->right){
+                return false;
+            }
+
+        }
+
+        return sp.size() == sq.size();
     }
 };
 
@@ -33,9 +69,15 @@ TEST(leetcode_100_same_tree, Basic)
 
     EXPECT_TRUE(solution->isSameTree(t1, t2));
     EXPECT_FALSE(solution->isSameTree(t1, t3));
+
+    EXPECT_TRUE(solution->isSameTree2(t1, t2));
+    EXPECT_FALSE(solution->isSameTree2(t1, t3));
 }
 
 int main(int argc, char *argv[]) {
+    srand((unsigned)time(NULL));
+    const time_t t = time(NULL);
+    cout << t << endl;
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
